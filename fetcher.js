e32350b7268e8812bf53a79ee1c1421e97966289
Teodor Talov <teodor@talov.com>
2013-12-01 21:50:14 -0500

    Fixed multiple issues with configuration and fetching data from solar pannels

--- fetcher.js
+++ fetcher.js
@@ -1,9 +1,9 @@
 const http = require('http'),
 	config = require('config'),
-	dbMapping = JSON.parse( config.Db.mapping ),
 	mongoose = require('mongoose'),
 	readingsModel = require('./models/readings.js').ReadingsModel;
 
+  config = JSON.stringify(config);
   mongoose.connect(config.Db.address);
 
   mongoose.connection.on('error', function(err){
@@ -11,20 +11,29 @@ const http = require('http'),
   });
 
 function fetch(){
+ 
 	http.get(config.Fetcher.url, fetchResult).on('error', function(e) {
-	  console.log("Got error: " + e.message);
+	  console.log("Got error: " + e.message)
 	});
 }
 
 function fetchResult( res ){
 	res.setEncoding('UTF-8');
-	res.on('data', function (data) {
-		data = JSON.parse(data);
-		writeToDb( data );
+	data = '';
+	res.on('data', function (chunk) {
+	 data += chunk;
+
 	});
+	res.on('end', function (res) {
+
+   writeToDb( data );
+//   res.end();
+  });
+
 }
 
 function remapColumns( d ){
+  var dbMapping = JSON.parse(config.Db.mapping);
 	for( var k in d.vals ){
 		var value = d.vals[k],
 			name = dbMapping[k];
@@ -33,14 +42,18 @@ function remapColumns( d ){
 	delete d.vals;
 }
 
-function writeToDb( data ){
+function writeToDb(data){
+ var data = JSON.parse(data);
+
 	for( var k in data.data ){
-		var d = data.data[k];
+
+	 var d = data.data[k];
 		d.time = new Date();
 		remapColumns( d );
 
 		var model = new readingsModel(d);
 		model.save(function(err){
+		 
 		  readingsModel.ensureIndexes(function(){
 		      console.log('ensure index');
 		  });
--- models/readings.js
+++ models/readings.js
@@ -2,7 +2,24 @@ const mongoose = require('mongoose');
 
 var readingsSchema = new mongoose.Schema({
   no:  Number,
-  voltage1: Number,
+  SEVolt_Avg: Number,
+  SEVolt_2_Avg: Number,
+  SEVolt_3_Avg: Number,
+  SEVolt_2_Tot: Number,
+  SEVolt_3_Tot: Number,
+  'SEVolt_4_Avg(2)': Number,
+  'SEVolt_4_Avg(3)': Number,
+  'SEVolt_4_Avg(4)': Number,
+  'SEVolt_4_Avg(5)': Number,
+  'SEVolt_4_Avg(6)': Number,
+  'SEVolt_4_Avg(7)': Number,
+  'SEVolt_4_Avg(8)': Number,
+  'SEVolt_4_Avg(9)': Number,
+  'SEVolt_4_Avg(10)': Number,
+  'SEVolt_4_Avg(11)': Number,
+  'SEVolt_4_Avg(12)': Number,
+  'SEVolt_4_Avg(13)': Number,
+  PTemp_C_Avg: Number,
   time: { type: Date, default: Date.now },
 });
 
